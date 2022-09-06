3b49e90636abfae9e531425492d25366ba74f955
Gilrivas <jorged1509@gmail.com>
2022-09-06 16:29:25 +0200

    Home section and Register section finished, waiting for validation

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlaBlaCampus</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/bbcLogo.svg">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="mainAccueil">
+        <div class="logo-container">
+            <div class="logo-img"><img src="assets/img/bbcLogo.svg" alt=""></div>
+            <div class="logo-Tittle">
+                <h1>BLABLA</h1>
+            </div>
+        </div>
+
+        <div class="startBtn" id="startBtn">
+            <a href="register.html">
+                <img src="assets/img/car-icon.svg" alt="">
+                <button>COMMENCER</button>
+            </a>
+        </div>
+
+        <div class="loginBtn" id="loginBtn">
+            <a href="login.html">
+                <button>SE CONNECTER</button>
+            </a>
+        </div>
+
+    </div>
+</body>
+</html>
