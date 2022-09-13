8977ee8e491fb1ab1203d408d16bcb2ec2b5458f
Jorge Gil Rivas <jorged1509@gmail.com>
2022-09-13 14:04:10 +0200

    page list.php ready for checking

--- list.php
+++ list.php
@@ -35,6 +35,71 @@
             <span class="phpNumber">2</span>
         </h2>
 
+        <div class="card-body">
+            <div class="travel-start">
+                <h3 class="hour">06H30</h3>
+                <img src="assets/img/circle-line.svg" alt="">
+                <h3 class="place">Dole</h3>
+            </div>
+            
+        <!--     <div class="travel-half">
+                <h3 class="hour">06H30</h3>
+                <img src="assets/img/circle-line.svg" alt="">
+                <h3 class="place">Dole</h3>
+            </div> -->
+          
+            <div class="travel-end">
+                <h3 class="hour">07H30</h3>
+                <img src="assets/img/circle.svg" alt="">
+                <h3 class="place">Lons-le-Saunier</h3>
+            </div>
+        </div>  
+        
+        <div class="profile-travel">
+         <img src="https://picsum.photos/200/300" alt="Photo de profil">
+        
+         <div class="user-travel">
+             <h2>PHP USERNAME</h2>
+             <h3>PHP BIO </h3>
+         </div>
+       </div>
+        
+
+    </div>
+    
+    <div class="card-trajet">
+        <h2>PLACES DISPONIBLES 
+            <span class="phpNumber">2</span>
+        </h2>
+
+        <div class="card-body">
+            <div class="travel-start">
+                <h3 class="hour">06H30</h3>
+                <img src="assets/img/circle-line.svg" alt="">
+                <h3 class="place">Dole</h3>
+            </div>
+
+            <div class="travel-half">
+                <h3 class="hour">06H30</h3>
+                <img src="assets/img/circle-line.svg" alt="">
+                <h3 class="place">Dole</h3>
+            </div>
+          
+            <div class="travel-end">
+                <h3 class="hour">07H30</h3>
+                <img src="assets/img/circle.svg" alt="">
+                <h3 class="place">Lons-le-Saunier</h3>
+            </div>
+        </div>  
+        
+        <div class="profile-travel">
+         <img src="https://picsum.photos/200/300" alt="Photo de profil">
+        
+         <div class="user-travel">
+             <h2>PHP USERNAME</h2>
+             <h3>PHP BIO </h3>
+         </div>
+       </div>
         
 
     </div>
