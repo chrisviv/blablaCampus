2ae080903812c7d756eeca00734ef533bd620e78
chris <c.vivancos@codeur.online>
2022-09-15 15:10:28 +0200

    rajout partie html/css validation de la réservation

---
+++ bookValidate.php
@@ -0,0 +1,30 @@
+<?php include('head.php') ?>
+
+<div class="bookVadilateMain">
+    <?php include('navbar.php')?>
+    <h1>VALIDATION DE LE RÉSERVATION</h1>
+</div>
+
+<div class="bookValidate">
+    <img class='redCircul'src="https://picsum.photos/200" alt="">
+    <div class="bookVadilateCircul">
+        
+        <p><span>Alain</span></p>
+        <p>Demande de réservation pour le trajet</p>
+        <p>Dole Lons le Saunier du 5 Semptembre 2022</p>
+    </div>
+</div>
+
+<div class="boxBookValidate">
+    <p> Bonjour <span> Alain </span></p>
+    <p>Je t’informe qu’une place t’attend dans ma voiture <br> pour le trajet <span> Dole - Lons le Saunie.</span></p>
+    <p>A bientot.</p>
+</div>
+</div>
+<div class="bookValidateBtn" id="bookValidateBtn">
+    <a href="list.php">
+        <button>VALIDER LA RÉSERVATION</button>
+    </a>
+</div>
+
+   
